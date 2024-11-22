d903916599ce5828a54d8da6771ffa6e0442a4e1
Hasibullah Wahabzadah <hasibullahwahabzadah@gmail.com>
2024-11-22 17:34:43 +0430

    update whatsapp link

--- public/js/script2.js
+++ public/js/script2.js
@@ -1,7 +1,5 @@
-
 const urlParams = new URLSearchParams(window.location.search);
 const jobId = urlParams.get('id');
-
 
 async function loadJobDetails() {
     const response = await fetch('/public/jobs.json');
@@ -9,20 +7,22 @@ async function loadJobDetails() {
     const job = jobsData.find(j => j.id === jobId);
 
     if (job) {
+        const whatsappLink = `https://api.whatsapp.com/send?phone=93794454095&text=Hello,%20I'm%20interested%20in%20the%20job%20"${job.title}"`;
+
         document.getElementById('job-details').innerHTML = `
-        <div class = "container1 ">
-            <div class = "first-section">
-                  <img class =" image" src="${job.image}" alt="${job.title}" >
-             <div>
-                  <h6 class="card-titles">${job.company}</h6>
-                  <p class="text-muteds" >${job.location}</p>
-             </div>
+        <div class="container1">
+            <div class="first-section">
+                <img class="image" src="${job.image}" alt="${job.title}">
+                <div>
+                    <h6 class="card-titles">${job.company}</h6>
+                    <p class="text-muteds">${job.location}</p>
+                </div>
             </div>
-                 <h2 class = "title">${job.title}</h2>
-                 <p class = "type"><strong>Type:</strong> ${job.type}</p>
-                 <p class = "salary"><strong>Salary:</strong> ${job.salary}</p>
-                 <p class = "description"> ${job.description}</p>
-                 <a class="btns" href="${job.id}">Apply Now</a>
+            <h2 class="title">${job.title}</h2>
+            <p class="type"><strong>Type:</strong> ${job.type}</p>
+            <p class="salary"><strong>Salary:</strong> ${job.salary}</p>
+            <p class="description">${job.description}</p>
+            <a class="btns" href="${whatsappLink}" target="_blank">Apply Now</a>
         </div>
         `;
     } else {
